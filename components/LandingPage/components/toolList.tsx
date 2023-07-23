f12c72f3d68b6d3f9ce0cb040f0ad47f9b3f10b0
Nosa-Drexx <nosakhare.egharevba@physci.uniben.edu>
2023-07-23 14:45:27 +0100

    tool section animated

--- components/LandingPage/components/toolList.tsx
+++ components/LandingPage/components/toolList.tsx
@@ -1,25 +1,78 @@
+"use client";
+import { useState } from "react";
 import styles from "../styles/toolList.module.css";
 
+import { Reorder, motion } from "framer-motion";
+
+const itemVariants = {
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: { type: "spring", stiffness: 300, damping: 24 },
+  },
+  hide: { opacity: 0, y: 20, transition: { duration: 0.2 } },
+};
+
+const variantContainer = {
+  show: {
+    transition: {
+      type: "spring",
+      bounce: 0,
+      duration: 0.7,
+      delayChildren: 0.3,
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const ToolItems = [
+  {
+    header: "Reporting & Analysis",
+    text: "Easy to understand stats",
+    color: "#f98a5d",
+  },
+  {
+    header: "Podcast Directory",
+    text: "Get listened Everywhere",
+    color: "#4a90e2",
+  },
+  {
+    header: "Monetization Tools",
+    text: "Listener support",
+    color: "#8b63c8",
+  },
+  {
+    header: "Publishing for Everyone",
+    text: "Easily create and publish episodes",
+    color: "#92c13c",
+  },
+];
+
 const ToolList = () => {
+  const [items, setItems] = useState(ToolItems);
   return (
-    <div className={styles.analysis}>
-      <div className={styles.analysisList}>
-        <h3 style={{ color: "#f98a5d" }}>Reporting & Analysis</h3>
-        <p>Easy to understand stats</p>
-      </div>
-      <div className={styles.analysisList}>
-        <h3 style={{ color: "#4a90e2" }}>Podcast Directory</h3>
-        <p>Get listened Everywhere</p>
-      </div>
-      <div className={styles.analysisList}>
-        <h3 style={{ color: "#8b63c8" }}>Monetization Tools</h3>
-        <p>Listner support</p>
-      </div>
-      <div className={styles.analysisList}>
-        <h3 style={{ color: "#92c13c" }}>Publishing for Everyone</h3>
-        <p>Easily create and publish episodes</p>
-      </div>
-    </div>
+    <motion.div
+      initial={"hide"}
+      variants={variantContainer}
+      whileInView={"show"}
+      className={styles.analysis}
+    >
+      {/* Framer motion Reorder Animation */}
+      <Reorder.Group axis="y" values={items} onReorder={setItems}>
+        {items.map((item) => (
+          <Reorder.Item
+            key={item.header}
+            value={item}
+            style={{ listStyleType: "none" }}
+          >
+            <motion.div variants={itemVariants} className={styles.analysisList}>
+              <h3 style={{ color: item.color }}>{item.header}</h3>
+              <p>{item.text}</p>
+            </motion.div>
+          </Reorder.Item>
+        ))}
+      </Reorder.Group>
+    </motion.div>
   );
 };
 
--- components/LandingPage/components/toolSection.tsx
+++ components/LandingPage/components/toolSection.tsx
@@ -1,29 +1,65 @@
+"use client";
 import Image from "next/image";
 import styles from "../styles/toolSection.module.css";
 import ToolList from "./toolList";
+import { motion } from "framer-motion";
+
+const mainTextVariant = {
+  inView: {
+    opacity: 1,
+    y: 0,
+    transition: { type: "spring", stiffness: 300, damping: 24 },
+  },
+  notInView: { opacity: 0, y: 20, transition: { duration: 0.2 } },
+};
+
+const imageVariant = {
+  inView: {
+    opacity: 1,
+    transition: { type: "spring", stiffness: 300, damping: 24 },
+  },
+  notInView: { opacity: 0, transition: { duration: 0.2 } },
+};
+
+const staggerVariant = {
+  inView: {
+    transition: {
+      type: "spring",
+      bounce: 0,
+      duration: 0.7,
+      delayChildren: 0.3,
+      staggerChildren: 0.05,
+    },
+  },
+};
 
 const ToolSection = () => {
   return (
-    <section id="tools" className={styles.toolSection}>
-      <div className={styles.toolsContainer}>
-        <div className={styles.toolSection}>
+    <motion.section
+      initial={"notInView"}
+      whileInView={"inView"}
+      id="tools"
+      className={styles.toolSection}
+    >
+      <motion.div variants={staggerVariant} className={styles.toolsContainer}>
+        <motion.div variants={mainTextVariant} className={styles.toolSection}>
           <h2>Easy and Powerful Tools</h2>
           <p>Everything you need to have a successful podcast</p>
-        </div>
-        <div className={styles.tools}>
-          <div className={styles.graphContainer}>
+        </motion.div>
+        <motion.div className={styles.tools}>
+          <motion.div variants={imageVariant} className={styles.graphContainer}>
             <Image
               className={styles.graph}
               src={require("../images/graph.png")}
               alt="graph"
             />
-          </div>
-          <div className={styles.toolList}>
+          </motion.div>
+          <motion.div className={styles.toolList}>
             <ToolList />
-          </div>
-        </div>
-      </div>
-    </section>
+          </motion.div>
+        </motion.div>
+      </motion.div>
+    </motion.section>
   );
 };
 
