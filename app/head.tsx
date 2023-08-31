03be556badc8f3beffb512885e402ed65882081e
Egharevba Nosa <96873980+Nosa-Drexx@users.noreply.github.com>
2023-08-31 08:17:01 +0100

    PWA update

--- app/head.tsx
+++ app/head.tsx
@@ -4,6 +4,58 @@ export default function Head() {
       <title>Jamit</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Jamit Landing page" />
+      <meta name="application-name" content="Jamit" />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+      <meta name="apple-mobile-web-app-title" content="Jamit web PWA" />
+      <meta name="description" content="Jamit Best PWA App in the world" />
+      <meta name="format-detection" content="telephone=no" />
+      <meta name="mobile-web-app-capable" content="yes" />
+      <meta name="msapplication-config" content="/icons/browserconfig.xml" />
+      <meta name="msapplication-TileColor" content="#2B5797" />
+      <meta name="msapplication-tap-highlight" content="no" />
+      <meta name="theme-color" content="#000000" />
+
+      <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
+      <link
+        rel="apple-touch-icon"
+        sizes="152x152"
+        href="/icons/touch-icon-ipad.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/icons/touch-icon-iphone-retina.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="167x167"
+        href="/icons/touch-icon-ipad-retina.png"
+      />
+
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/icons/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/icons/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/manifest.json" />
+      <link
+        rel="mask-icon"
+        href="/icons/safari-pinned-tab.svg"
+        color="#5bbad5"
+      />
+      <link rel="shortcut icon" href="/favicon.ico" />
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
+      />
       <link rel="icon" href="/favicon.ico" />
       <script
         defer
